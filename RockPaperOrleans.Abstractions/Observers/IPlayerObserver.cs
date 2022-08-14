7f1477cb9a458fbc69d614e6c682b842babba34f
bradygaster <bradyg@microsoft.com>
2022-08-13 19:34:40 -0700

    added handlers

--- RockPaperOrleans.Abstractions/Observers/IPlayerObserver.cs
+++ RockPaperOrleans.Abstractions/Observers/IPlayerObserver.cs
@@ -4,9 +4,11 @@ namespace RockPaperOrleans.Abstractions
 {
     public interface IPlayerObserver : IGrainObserver
     {
+        Task<Play> Go();
         Task OnPlayerSignedIn(Player player);
         Task OnPlayerSignedOut(Player player);
         Task OnOpponentSelected(Player opponent);
-        Task<Play> Go();
+        Task OnGameWon(Player player);
+        Task OnGameLost(Player player);
     }
 }
