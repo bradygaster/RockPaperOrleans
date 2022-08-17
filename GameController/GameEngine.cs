2159e36e0b1249bc924f5bdb3a428aeddfb06aa1
bradygaster <bradyg@microsoft.com>
2022-08-16 17:01:47 -0700

    added players project to try multiple players in one project - it works.

--- GameController/GameEngine.cs
+++ GameController/GameEngine.cs
@@ -44,7 +44,7 @@ namespace GameController
                     }
                 }
 
-                await Task.Delay(5000);
+                await Task.Delay(1000);
             }
         }
     }
---
+++ Players/Program.cs
@@ -0,0 +1,41 @@
+using RockPaperOrleans;
+using RockPaperOrleans.Abstractions;
+
+await Task.Delay(30000); // for debugging, give the silo time to warm up
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .UseOrleans((context, siloBuilder) =>
+    {
+        siloBuilder
+            .PlayRockPaperOrleans(context.Configuration)
+            .EnlistPlayer<AlwaysPaper>()
+            .EnlistPlayer<AlwaysRock>()
+            .EnlistPlayer<AlwaysScissors>();
+    })
+    .Build();
+
+await host.RunAsync();
+
+public class AlwaysPaper : PlayerBase
+{
+    public AlwaysPaper(ILogger<AlwaysPaper> logger) : base(logger) { }
+
+    public override Task<Play> Go()
+        => Task.FromResult(Play.Paper);
+}
+
+public class AlwaysRock : PlayerBase
+{
+    public AlwaysRock(ILogger<AlwaysRock> logger) : base(logger) { }
+
+    public override Task<Play> Go()
+        => Task.FromResult(Play.Rock);
+}
+
+public class AlwaysScissors : PlayerBase
+{
+    public AlwaysScissors(ILogger<AlwaysScissors> logger) : base(logger) { }
+
+    public override Task<Play> Go()
+        => Task.FromResult(Play.Scissors);
+}
